b9f8926f976b8e202e11db7aafa93e4a934788ae
LeiMing627 <jackperry933jp@gmail.com>
2019-11-25 16:02:05 +0100

    linter, formatter

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import './App.css';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Hello world
-      </header>
-    </div>
-  );
-}
+    return (
+        <div className="App">
+            <header className="App-header">Hello world</header>
+        </div>
+    );
+};
 
 export default App;
