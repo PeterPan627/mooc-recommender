72584f34934dd0f9ebed6f67a13db34f3fd423d8
LeiMing627 <jackperry933jp@gmail.com>
2019-11-25 14:53:34 +0100

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello world
+      </header>
+    </div>
+  );
+}
+
+export default App;
